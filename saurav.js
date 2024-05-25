88d3bd5fabc462e147b907299f86d83f14615ceb
iasaurav <101176678+iasaurav@users.noreply.github.com>
2024-05-25 14:37:50 +0530

    saurav.js

--- saurav.js
+++ saurav.js
@@ -1,7 +1,4 @@
 const urls=['facebook.com/iasaurav','instagram.com/ia_aurav','twitter.com/ia_aurav','t.me/ia_aurav','youtube.com/@Iasaurav','pinterest.com/iasaurav','medium.com/@ia_aurav','codepen.io/ia_aurav','github.com/iasaurav',];
-  
-  
-  
 $.each(urls, function(j, data) {
 var ul_dat = ['<a href="https://'+urls[j]+'">'+urls[j]+'</a></br>'];
             $("#ioplsnzn").append(ul_dat);
