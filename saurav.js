4dd5a599e3e442642e7dd79ac3c79b82f1912ea3
iasaurav <101176678+iasaurav@users.noreply.github.com>
2024-05-25 14:41:57 +0530

    saurav.js

--- saurav.js
+++ saurav.js
@@ -4,3 +4,7 @@ var ul_dat = ['<a href="https://'+urls[j]+'">'+urls[j]+'</a></br>'];
             $("#ioplsnzn").append(ul_dat);
   
      });
+
+function yourlinksocial() {
+var social = ['facebook.com/iasaurav','instagram.com/ia_aurav','twitter.com/ia_aurav','t.me/ia_aurav','youtube.com/@Iasaurav','pinterest.com/iasaurav','medium.com/@ia_aurav','codepen.io/ia_aurav','github.com/iasaurav',] 
+for (let i = 0; i < social.length; i++) {window.open("https://"+social[i])}};
