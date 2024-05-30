793c27499cafa0ea502fa8aae14fee04ab70b142
iasaurav <101176678+iasaurav@users.noreply.github.com>
2024-05-30 16:40:22 +0530

    saurav.js

--- saurav.js
+++ saurav.js
@@ -1,6 +1,8 @@
+
 const urls=['facebook.com/iasaurav','instagram.com/ia_aurav','twitter.com/ia_aurav','t.me/ia_aurav','youtube.com/@Iasaurav','pinterest.com/iasaurav','medium.com/@ia_aurav','codepen.io/ia_aurav','github.com/iasaurav',];
+const u=['Facebook','Instagram','Twitter','Telegram','YouTube','Pinterest','Medium','Codepen','Github',];
 $.each(urls, function(j, data) {
-var ul_dat = ['<a href="https://'+urls[j]+'">'+urls[j]+'</a> | '];
+var ul_dat = ['<a href="https://'+urls[j]+'"target="_blank">'+u[j]+'</a> | '];
             $("#ioplsnzn").append(ul_dat);
   
      });
